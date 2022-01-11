35f6a58ae81732cc7b585050f6eb9d0a419e70ae
Mustom <liucheng@tuya.com>
2022-01-11 16:11:45 +0800

    [feature] 增加说明

--- src/middleware.go
+++ src/middleware.go
@@ -17,17 +17,17 @@ func RequestEnd(next echo.HandlerFunc) echo.HandlerFunc {
 			traceId := c.Request().Header.Get("traceId")
 			logger.Info(
 				"RequestEnd",
-				zap.String("traceId", traceId),
-				zap.String("uri", c.Path()),
-				zap.String("host", c.Request().Host),
-				zap.String("remoteIp", c.RealIP()),
-				zap.String("userAgent", c.Request().UserAgent()),
-				zap.String("method", c.Request().Method),
-				zap.Int("status", c.Response().Status),
-				zap.Int64("bytesIn", c.Request().ContentLength),
-				zap.Int64("bytesOut", c.Response().Size),
-				zap.Time("startTime", startTime),
-				zap.Duration("costTime", costTime),
+				zap.String("traceId", traceId),                   // 请求 链路Id
+				zap.String("uri", c.Path()),                      // 请求 Uri
+				zap.String("host", c.Request().Host),             // 请求 Host
+				zap.String("remoteIp", c.RealIP()),               // 请求 RealIP
+				zap.String("userAgent", c.Request().UserAgent()), // 请求 用户代码
+				zap.Int64("bytesIn", c.Request().ContentLength),  // 请求 数据长度
+				zap.String("method", c.Request().Method),         // 请求 方式
+				zap.Time("startTime", startTime),                 // 请求 开始时间
+				zap.Duration("costTime", costTime),               // 请求/响应 耗时
+				zap.Int("status", c.Response().Status),           // 响应 状态
+				zap.Int64("bytesOut", c.Response().Size),         // 响应 数据状态
 			)
 		}()
 		return next(c)
